bb1b64365cb0f9c4cda2d24c24750075f8a6b56a
sergey <sbarchshevskiy@gmail.com>
2021-03-15 20:13:07 -0400

    unit test for available spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -10,17 +10,22 @@ export default function DayListItem(props) {
   
 
   const formatSpots = function (data) {
-    data = props.spots;
-    return `${data} spots available`
-  }
 
-  console.log('format spots' , formatSpots);
+    if (data === 1) {
+      return `${data} spot remaining`
+    } else if (data === 0){
+      return `no spots remaining`
+    } else {
+      return `${data} spots remaining`
+    }
+  
+  }
 
 
   return (
   
     <li onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.spots}</h2>
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
 
