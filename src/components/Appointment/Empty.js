f11ff1a3e7952b509edfb8e7cdf255cc52ed8d17
sergey <sbarchshevskiy@gmail.com>
2021-03-16 21:24:06 -0400

    works on appointment show, confirm, empty, header

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,20 @@
+import React from "react";
+const classNames = require('classnames');
+
+
+export default function Empty(props) {
+  // const EmptyClass = classNames("appointment__add", {
+  //   "appointment__add-button": props.onAdd
+  // });
+
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+        onClick={props.onAdd}
+      />
+    </main>
+  )
+}
